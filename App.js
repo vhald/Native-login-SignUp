c2ac39a34bc40488ef18c20cce2fd17a30a8e160
vhald <vaibhavgsh@gmail.com>
2022-03-17 23:13:30 +0530

    app.js created

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+// screens
+import Login from './screens/Login';
+import SignUp from './screens/Signup';
+import Welcome from './screens/Welcome';
+
+export default function App() {
+  return (
+    
+    <SignUp />
+    // <View style={styles.container}>
+    //   {/* below line was for hello world and button demo */}
+    //    {/* <Text>Hello world!</Text>
+    //   <Button title="Alerty" onPress={()=> alert("Kya haal chaal")}/> */}
+    //   <StatusBar style="auto" />
+    // </View>
+
+    );
+}
+
+ 
